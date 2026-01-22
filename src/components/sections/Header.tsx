526a90aa3fc00fcfd130d7b0d91c291e5da69263
faustoalves <fausto@faustoalves.com.br>
2026-01-21 23:03:43 -0300

    With correct date

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -9,8 +9,6 @@ type Props = {}
 
 const Header = (props: Props) => {
   const dateTime = new Date();
-  dateTime.setHours(10, 0, 0, 0);
-  dateTime.setDate(11);
   const daySchedule = getHeaderByDateTime(dateTime);
   return (
     <section className='w-full aspect-8/4 md:aspect-video lg:aspect-16/8 xl:aspect-16/7 mt-4 flex flex-col items-center justify-center relative top-line bottom-line pattern-bg p-2 '>
