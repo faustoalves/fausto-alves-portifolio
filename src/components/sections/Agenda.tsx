bf3958b136d2b815a19da3aed4a8c98866d2c50b
faustoalves <fausto@faustoalves.com.br>
2026-01-22 16:02:39 -0300

    BAse

--- src/components/sections/Agenda.tsx
+++ src/components/sections/Agenda.tsx
@@ -4,8 +4,24 @@ type Props = {}
 
 const Agenda = (props: Props) => {
   return (
-    <section className='w-full flex flex-col items-start justify-center relative py-4 mt-10 top-line bottom-line '>
-      <h2 className='text-2xl font-bold'>Agenda</h2>
+    <section className='w-full flex flex-col items-start justify-center relative mt-10 top-line bottom-line '>
+    <div className='w-full bottom-line pattern-bg lg:col-span-2 p-4 text-center relative'>
+        <h2>Let's work together</h2>
+      </div>
+      <div className='w-full h-full flex flex-col items-start justify-center my-10'>
+
+      </div>
+      {/* <div className="absolute top-0 right-0 left-0 2xl:right-[50%] 2xl:left-[50%] min-w-full 2xl:translate-x-[-50%] 2xl:w-screen flex items-end justify-end h-full bg-pink-100 dark:bg-pink-950">
+        <div className="w-full h-full  max-w-[1456px] mx-auto bg-pink-200 dark:bg-pink-950 p-4"> 
+          <h2 className="text-2xl font-bold">
+          Works
+        </h2>
+      </div>
+
+      </div> */}
+      
+
+
     </section>
   )
 }
--- src/components/sections/Works.tsx
+++ src/components/sections/Works.tsx
@@ -8,27 +8,26 @@ const Works = (props: Props) => {
 
   return (
     <>
-    <section
-      className="relative w-full flex flex-col items-start justify-center p-4 mt-10 top-line bottom-line min-h-96 "
-    >
+    <section className='w-full flex flex-col items-start justify-center relative mt-10 top-line bottom-line '>
+    <div className='w-full bottom-line pattern-bg lg:col-span-2 p-4 text-center relative'>
+        <h2>Last works</h2>
+      </div>
+      <div className='w-full h-full flex flex-col items-start justify-center my-10'>
 
-      <div className="absolute top-0 right-0 left-0 2xl:right-[50%] 2xl:left-[50%] min-w-full 2xl:translate-x-[-50%] 2xl:w-screen flex items-end justify-end h-full bg-pink-100 dark:bg-pink-950">
+      </div>
+      {/* <div className="absolute top-0 right-0 left-0 2xl:right-[50%] 2xl:left-[50%] min-w-full 2xl:translate-x-[-50%] 2xl:w-screen flex items-end justify-end h-full bg-pink-100 dark:bg-pink-950">
         <div className="w-full h-full  max-w-[1456px] mx-auto bg-pink-200 dark:bg-pink-950 p-4"> 
           <h2 className="text-2xl font-bold">
           Works
         </h2>
       </div>
 
-      </div>
+      </div> */}
       
 
 
     </section>
-    <div className="relative w-full h-16  flex flex-row justify-between">
-    <Button label='Download CV' className='w-1/4 h-16' />
 
-
-    </div>
 </>
   );
 }
