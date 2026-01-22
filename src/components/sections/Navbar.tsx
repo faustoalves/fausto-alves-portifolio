6f1f34b5fd9f8c6d21f64c980cdb7c7ce5ea18de
faustoalves <fausto@faustoalves.com.br>
2026-01-22 11:05:12 -0300

    Fix Navbar

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -9,9 +9,7 @@ const Navbar = (props: Props) => {
     <nav className='w-full flex items-center justify-between relative p-4 top-line bottom-line bg-purple-50 dark:bg-purple-900 mx '>
       <LogoFausto className='text-pink-700 dark:text-pink-100 h-8 w-auto' />
       <div className='flex items-center gap-4'>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/contact'>Contact</Link>
+        
       </div>
     </nav>
   )
