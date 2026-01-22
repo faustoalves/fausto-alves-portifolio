c6e4e78bb4ced081a5dfc7ab0fbd8e0f70999ffa
faustoalves <fausto@faustoalves.com.br>
2026-01-22 14:54:01 -0300

    Test without header

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -13,7 +13,7 @@ const Header = (props: Props) => {
   return (
     <section className='w-full aspect-8/4 md:aspect-video lg:aspect-16/8 xl:aspect-16/7 mt-4 flex flex-col items-center justify-center relative top-line bottom-line pattern-bg p-2 '>
       <div className='w-full h-full flex flex-col items-center justify-center bg-purple-50 dark:bg-purple-900 rounded-lg border-line relative'>
-        {daySchedule && <HeaderImageContainer {...daySchedule} />}
+        {/* {daySchedule && <HeaderImageContainer {...daySchedule} />} */}
 
       </div>
     </section>
