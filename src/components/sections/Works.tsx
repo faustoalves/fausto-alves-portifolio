b917e309c33e0f601f77fb2f52e9c4807d1e9aa9
faustoalves <fausto@faustoalves.com.br>
2026-01-22 20:59:19 -0300

    test

--- src/components/sections/Works.tsx
+++ src/components/sections/Works.tsx
@@ -34,10 +34,9 @@ const Works = (props: Props) => {
     <>
     <section
       ref={container}
-      className='w-full flex flex-col items-start justify-center relative  mt-10 top-line bottom-line '
+      className='w-full flex flex-col items-start justify-center relative mt-10 top-line bottom-line '
     >
-
-      <div className='w-full h-full flex flex-col items-start justify-center my-10 sticky top-0 gap-4 pb-60'>
+      <div className='w-full h-full flex flex-col items-start justify-center mb-10 sticky top-0 gap-4 pb-60'>
         <div className='w-full bottom-line pattern-bg lg:col-span-2 p-4 text-center sticky top-0'>
           <h2>Last works</h2>
         </div>
