9bd78dc5b38d58a2924fda0ef8fec10bddeec3d6
faustoalves <fausto@faustoalves.com.br>
2026-01-28 17:46:11 -0300

    Test Without image

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,19 +1,18 @@
-import { getHeaderByDateTime } from '@/lib/header'
-import HeaderImageContainer from '../header/HeaderImageContainer'
+import { getHeaderByDateTime } from "@/lib/header";
+import HeaderImageContainer from "../header/HeaderImageContainer";
 
-type Props = {}
+type Props = {};
 
 const Header = (props: Props) => {
   const dateTime = new Date();
   const daySchedule = getHeaderByDateTime(dateTime);
   return (
-    <section className='w-full aspect-8/4 md:aspect-video lg:aspect-16/8 xl:aspect-16/7 mt-4 flex flex-col items-center justify-center relative top-line bottom-line pattern-bg p-2 '>
-      <div className='w-full h-full flex flex-col items-center justify-center bg-purple-50 dark:bg-purple-900 rounded-lg border-line relative'>
-        {daySchedule && <HeaderImageContainer {...daySchedule} />}
-
+    <section className="w-full aspect-8/4 md:aspect-video lg:aspect-16/8 xl:aspect-16/7 mt-4 flex flex-col items-center justify-center relative top-line bottom-line pattern-bg p-2 ">
+      <div className="w-full h-full flex flex-col items-center justify-center bg-purple-50 dark:bg-purple-900 rounded-lg border-line relative">
+        {/* {daySchedule && <HeaderImageContainer {...daySchedule} />} */}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
