ff48e2880e056109c86602abd51a446a13115aa1
faustoalves <fausto@faustoalves.com.br>
2026-01-28 22:21:28 -0300

    Test Header Time

--- src/components/header/HeaderImageContainer.tsx
+++ src/components/header/HeaderImageContainer.tsx
@@ -1,61 +1,55 @@
-"use client"
-import type { DayScheduleItem } from '@/lib/header'
-import { CldImage } from 'next-cloudinary'
-import React from 'react'
-import { useMousePercent } from '@/hooks/MousePercent'
+"use client";
+import type { DayScheduleItem } from "@/lib/header";
+import { CldImage } from "next-cloudinary";
+import React from "react";
+import { useMousePercent } from "@/hooks/MousePercent";
 
 type Props = {
-  daySchedule: DayScheduleItem
-}
+  daySchedule: DayScheduleItem;
+};
 
 const HeaderImageContainer = ({ imageFront, imageBg }: DayScheduleItem) => {
-
   const mousePercent = useMousePercent();
-
-
-
-
   return (
-
-    
-
-
-    <div className='w-full h-full'>
-      <div className='w-full h-[calc(125%)] absolute rounded-lg top-[-25%] overflow-hidden '>
-      {imageFront && <CldImage
-          width="1600"
-          height="884"
-          src={imageFront}
-          sizes="(max-width: 768px) 100vw,
+    <div className="w-full h-full">
+      <div className="w-full h-[calc(125%)] absolute rounded-lg top-[-25%] overflow-hidden ">
+        {imageFront && (
+          <CldImage
+            width="1600"
+            height="884"
+            src={imageFront}
+            sizes="(max-width: 768px) 100vw,
           (max-width: 1200px) 50vw,
           33vw"
-          alt="Description of my image"
-        quality={75}
-
-          className='w-full h-full relative z-20 object-cover scale-115 md:scale-[107%] md:pt-10'
-          style={{
-            transform: `translate(${mousePercent.x * 50 - 25}px) translateY(${mousePercent.y * 50 }px)`,
-          }}
-        />}
+            alt="Description of my image"
+            quality={75}
+            className="w-full h-full relative z-20 object-cover scale-115 md:scale-[107%] md:pt-10"
+            style={{
+              transform: `translate(${mousePercent.x * 50 - 25}px) translateY(${mousePercent.y * 50}px)`,
+            }}
+          />
+        )}
       </div>
-      <div className='w-full h-full block  overflow-hidden rounded-lg '>
-      {imageFront && <CldImage
-          width="1519"
-          height="700"
-          src={imageBg}
-          sizes="(max-width: 768px) 100vw,
+      <div className="w-full h-full block  overflow-hidden rounded-lg ">
+        {imageFront && (
+          <CldImage
+            width="1519"
+            height="700"
+            src={imageBg}
+            sizes="(max-width: 768px) 100vw,
           (max-width: 1200px) 50vw,
           33vw"
-        quality={75}
-          alt="Description of my image"
-          className='w-full h-full object-cover scale-115 md:scale-[107%]'
-          style={{
-            transform: `translate(${mousePercent.x * 30 - 15}px) translateY(${mousePercent.y * 30 - 15}px)`,
-          }}
-        />}
+            quality={75}
+            alt="Description of my image"
+            className="w-full h-full object-cover scale-115 md:scale-[107%]"
+            style={{
+              transform: `translate(${mousePercent.x * 30 - 15}px) translateY(${mousePercent.y * 30 - 15}px)`,
+            }}
+          />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderImageContainer
+export default HeaderImageContainer;
--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -5,11 +5,15 @@ type Props = {};
 
 const Header = (props: Props) => {
   const dateTime = new Date();
+  dateTime.setHours(dateTime.getHours() - 4);
   const daySchedule = getHeaderByDateTime(dateTime);
   return (
     <section className="w-full aspect-8/4 md:aspect-video lg:aspect-16/8 xl:aspect-16/7 mt-4 flex flex-col items-center justify-center relative top-line bottom-line pattern-bg p-2 ">
       <div className="w-full h-full flex flex-col items-center justify-center bg-purple-50 dark:bg-purple-900 rounded-lg border-line relative">
         {daySchedule && <HeaderImageContainer {...daySchedule} />}
+        <div className="text-xl text-purple-800 dark:text-white absolute bottom-0 right-0 p-2 z-60">
+          {dateTime.toLocaleString()}
+        </div>
       </div>
     </section>
   );
