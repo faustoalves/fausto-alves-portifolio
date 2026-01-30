bf74684b6e093f5cd1b2d4b6e0407d8cd3a63a8b
Fausto Alves <fausto@faustoalves.com.br>
2026-01-30 00:16:28 -0300

    Footer and Meta

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import FaIcon from "../common/logo/FaIcon";
+import LogoFausto from "../common/logo/LogoFausto";
 
 type Props = {};
 
@@ -12,8 +14,19 @@ const Footer = (props: Props) => {
 
       <div className="relative h-[calc(100vh+400px)] -top-[100vh]">
         <div className="h-[400px] sticky top-[calc(100vh-400px)] bg-linear-to-b from-purple-200 to-purple-50 dark:from-purple-800 dark:to-purple-900">
-          <div className="w-full max-w-[1536px] mx-auto h-full flex items-center justify-center">
-            <h1>Footer</h1>
+          <div className="w-full max-w-[1536px] mx-auto h-full flex flex-col items-center justify-center relative">
+            <FaIcon className="min-w-[520px] h-[520px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-purple-200 dark:text-purple-800 blur" />
+            <div className="flex flex-row items-center justify-Between w-full h-full z-1">
+              <LogoFausto className="h-20 w-auto  text-purple-800 dark:text-purple-100" />
+              <div className="flex flex-col items-center justify-center">
+                {/* Include social networks */}
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-center w-full h-18 border-t border-purple-700/30 dark:border-purple-500/30 z-1">
+              <p className="text-sm text-purple-600 dark:text-purple-400 text-center uppercase tracking-widest">
+                2026 - Todos os direitos reservados
+              </p>
+            </div>
           </div>
         </div>
       </div>
